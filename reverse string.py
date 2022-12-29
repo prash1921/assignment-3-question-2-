25e07a06ab9e0b09b89d3dbe8558d26678ffd97d
prash1921 <120668760+prash1921@users.noreply.github.com>
2022-12-29 12:37:31 +0530

    Add files via upload

---
+++ reverse string.py
@@ -0,0 +1,8 @@
+def string_reverse(str1):
+    rstr =''
+    index = len(str1)
+    while index > 0:
+        rstr +=str[index - 1 ]
+        index = index -1
+        return rstr1
+        print(string_reverse('1234abcd'))
